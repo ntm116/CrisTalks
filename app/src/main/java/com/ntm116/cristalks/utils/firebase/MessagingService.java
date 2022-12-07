2e87dc73ed8fc82655aca8255ebbbe1f42d81318
ntm116 <106641856+ntm116@users.noreply.github.com>
2022-12-07 20:53:32 +0700

    CHAT-3: Setup Firebase Cloud Messaging (#3)

---
+++ app/src/main/java/com/ntm116/cristalks/utils/firebase/MessagingService.java
@@ -0,0 +1,23 @@
+package com.ntm116.cristalks.utils.firebase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+    public MessagingService() {
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+    }
+}
