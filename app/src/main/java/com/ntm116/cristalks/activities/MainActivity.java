36551d2cec0b0a2dc9fe0ef95734e073e31e65e1
themanh01 <manhnt@metacity.email>
2022-12-10 17:59:41 +0700

    CHAT-7: Working with FCM

--- app/src/main/java/com/ntm116/cristalks/activities/MainActivity.java
+++ app/src/main/java/com/ntm116/cristalks/activities/MainActivity.java
@@ -2,15 +2,61 @@ package com.ntm116.cristalks.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.ntm116.cristalks.R;
+import com.ntm116.cristalks.databinding.ActivityMainBinding;
+import com.ntm116.cristalks.utils.Constants;
+import com.ntm116.cristalks.utils.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setContentView(binding.getRoot());
+        loadUserDetails();
+        getToken();
+    }
+
+    private void loadUserDetails()
+    {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+        binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken()
+    {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token)
+    {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                db.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+                .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 }
