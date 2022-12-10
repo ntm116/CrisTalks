3667edda4101f768695c8b7c788bd26d54d9e205
themanh01 <manhnt@metacity.email>
2022-12-10 22:46:01 +0700

    CHAT-10: Chat Screen Layout Design

---
+++ app/src/main/java/com/ntm116/cristalks/activities/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.ntm116.cristalks.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(null);
+    }
+}
